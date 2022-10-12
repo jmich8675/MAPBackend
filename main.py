3c75e5fc20599e3cc1cf4269f14e2131d09f2d20
B8man <70527765+B8man@users.noreply.github.com>
2022-10-12 18:18:58 -0400

    fixed minor jwt errors, added template creation
    known issue: get_preset_template does not return questions associated with template

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import re
 from urllib import request
 from fastapi import FastAPI, Response, status, Request
 from pydantic import BaseModel
-from starlette.responses import RedirectResponse,JSONResponse
+from starlette.responses import RedirectResponse, JSONResponse
 import starlette.status as status
 from fastapi.middleware.cors import CORSMiddleware
-import bcrypt
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from functools import wraps
+import bcrypt
 
 #DATABASE
 from fastapi import Depends, FastAPI, HTTPException
@@ -31,7 +31,7 @@ app = FastAPI()
 
 #CORS STUFF
 origins = [
-    "https://real-lemons-sleep-128-210-106-73.loca.lt",
+    "https://thick-peaches-shop-73-145-245-180.loca.lt",
     "http://localhost",
     "http://localhost:8000",
     "https://localhost:8000",
@@ -87,7 +87,7 @@ def verify_access_token(token: str, username: str):
     except JWTError:
         return False
     return True
-    
+
 @app.middleware("http")
 async def is_logged_in(request: Request, call_next):
     # credentials_exception = HTTPException(
@@ -95,7 +95,7 @@ async def is_logged_in(request: Request, call_next):
     #     detail="Could not validate credentials",
     #     headers={"WWW-Authenticate": "Bearer"},
     # )
-    urls = ["/login", "/signup", "/", "/docs", "/openapi.json", "/redoc"]
+    urls = ["/login", "/signup", "/", "/docs", "/openapi.json", "/redoc","/create_template"]
     print(request.url.path)
     if (request.url.path not in urls):
         token = request.headers.get('Authorization', None)
@@ -197,15 +197,57 @@ def login(user: User, response: Response, db: Session = Depends(get_db)):
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    
-    message = {"message": "User loggedin Successfully",
+    message = {"message": "User logged in successfully",
                 "username": user.username,
                 "access_token": access_token, 
                 "token_type": "bearer"}
     response.status_code = status.HTTP_200_OK
     return message  
 
-# verify JWT before accesssing this page
+
 @app.get("/{username}")
-def home(username: str, request: Request):
+def home(username: str):
     return {"home": username}
+
+
+@app.post("/{username}/create_specific_goal")
+def create_specific_goal(username: str, goal: schemas.GoalSpecificCreate,
+                response: Response, db: Session=Depends(get_db)):
+    #session authentication
+    user = crud.get_user_by_username(db=db, username=username)
+    if not user:
+        message = {"message": "User not found"}
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return message
+    creator_id = user.id
+    crud.create_specific_goal(db=db, goal=goal, user_id=creator_id)
+    template=crud.get_template(db=db, template_id=goal.template_id)
+    if not template:
+        message = {"message": "error: template not found"}
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return message
+    message = {"message": "Goal successfully created!"}
+    response.status_code = status.HTTP_200_OK
+    return message
+
+@app.post("/create_template")
+def create_template(username: str, template: schemas.TemplateCreate,
+                    response: Response, db: Session=Depends(get_db)):
+    user = crud.get_user_by_username(db=db, username=username)
+    if not user:
+        message = {"message": "error: user not found"}
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return message
+    creator_id = user.id
+    crud.create_template(db=db, template=template, creator_id=creator_id)
+    message = {"message": "template successfully created!"}
+    return message
+
+
+@app.get("/{username}/goals")
+def view_goals():
+    return "kazoink!"
+
+@app.get("/{username}/templates")
+def view_premade_templates(db: Session=Depends(get_db), skip: int = 0, limit: int = 100):
+    return crud.get_premade_templates(db=db, skip=skip, limit=limit)
--- schemas.py
+++ schemas.py
@@ -4,20 +4,6 @@ import enum
 
 from sqlalchemy import Integer, Interval
 
-class TemplateBase(BaseModel):
-    name: str
-    
-class TemplateCreate(TemplateBase):
-    pass
-
-class Template(TemplateBase):
-    is_custom: bool
-    template_id: int
-    creator_id: int
-
-    class Config:
-        orm_mode = True
-
 class ResponseBase(BaseModel):
     text: str
     question_id: int
@@ -33,11 +19,53 @@ class Response(ResponseBase):
     class Config:
         orm_mode = True
 
+
+class QuestionBase(BaseModel):
+    text: str
+    response_type: enum.Enum
+
+class QuestionCreate(QuestionBase):
+    pass
+
+class Question(QuestionBase):
+    question_id: int
+    template_id: int
+    check_in_num: int
+    next_check_in_period: timedelta
+    answers: list[Response] = []
+
+    class Config:
+        orm_mode = True
+
+
+class TemplateBase(BaseModel):
+    name: str
+    is_custom: bool
+    
+class TemplateCreate(TemplateBase):
+    pass
+
+class Template(TemplateBase):
+    template_id: int
+    creator_id: int
+    questions: list[Question] = []
+
+    class Config:
+        orm_mode = True
+
+
+
+
+
 class GoalBase(BaseModel):
     goal_name: str
-    check_in_period: timedelta
+    next_check_in: date
 
-class GoalCreate(GoalBase):
+class GoalSpecificCreate(GoalBase):
+    template_id: int
+    pass
+
+class GoalCustomCreate(GoalBase):
     pass
 
 class Goal(GoalBase):
@@ -45,7 +73,7 @@ class Goal(GoalBase):
     creator_id: int
     is_paused: bool
     start_date: date
-    next_check_in: date
+    check_in_period: timedelta
     check_in_num: int
     is_public: bool
     answers: list[Response] = []
@@ -69,21 +97,3 @@ class User(UserBase):
     class Config:
         orm_mode = True
 
-
-
-class QuestionBase(BaseModel):
-    text: str
-    response_type: enum.Enum
-
-class QuestionCreate(QuestionBase):
-    pass
-
-class Question(QuestionBase):
-    question_id: int
-    template_id: int
-    check_in_num: int
-    next_check_in_period: timedelta
-    answers: list[Response] = []
-
-    class Config:
-        orm_mode = True
