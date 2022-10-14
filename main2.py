bdeb20f8224d3f356e09c4b8bb35ad8509eef8b8
mashur <mashur@pop-os.localdomain>
2022-10-13 22:51:02 -0400

    fixed cors and checking authentication

--- main2.py
+++ main2.py
@@ -3,13 +3,18 @@ import re
 from urllib import request
 from fastapi import FastAPI, Response, status, Request
 from pydantic import BaseModel
-from starlette.responses import RedirectResponse,JSONResponse
+from starlette.responses import RedirectResponse, JSONResponse
 import starlette.status as status
 from fastapi.middleware.cors import CORSMiddleware
-import bcrypt
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.middleware import Middleware
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from functools import wraps
+import bcrypt
+from fastapi.routing import APIRoute
+from fastapi.exceptions import RequestValidationError
+
 #DATABASE
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
@@ -27,25 +32,6 @@ def get_db():
 #DATABASE
 
 app = FastAPI()
-
-#CORS STUFF
-origins = [
-    "https://real-lemons-sleep-128-210-106-73.loca.lt",
-    "http://localhost",
-    "http://localhost:8000",
-    "https://localhost:8000",
-    "https://localhost:3000",
-    "http://localhost:3000"
-    ]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-#CORS STUFF
 
 #REQUEST MODELS
 class User(BaseModel):
@@ -86,28 +72,55 @@ def verify_access_token(token: str, username: str):
     except JWTError:
         return False
     return True
-    
-@app.middleware("http")
-async def is_logged_in(request: Request, call_next):
-    # credentials_exception = HTTPException(
-    #     status_code=status.HTTP_401_UNAUTHORIZED,
-    #     detail="Could not validate credentials",
-    #     headers={"WWW-Authenticate": "Bearer"},
-    # )
-    urls = ["/login", "/signup", "/", "/docs", "/openapi.json", "/redoc"]
-    print(request.url.path)
-    if (request.url.path not in urls):
-        token = request.headers.get('Authorization', None)
-        username = request.url.path.split('/')[1]
 
-        if (not token) or (not verify_access_token(token, username)):
-            return JSONResponse(content={
-                    "message": "User Not logged in"
-                }, status_code=status.HTTP_400_BAD_REQUEST)
 
-    response = await call_next(request)
-    return response
+class IsLoggedIn(APIRoute):
+    def get_route_handler(self) -> callable:
+        original_route_handler = super().get_route_handler()
+
+        async def route_authentication_handler(request: Request) -> Response:
+            # credentials_exception = HTTPException(
+            #     status_code=status.HTTP_401_UNAUTHORIZED,
+            #     detail="Could not validate credentials",
+            #     headers={"WWW-Authenticate": "Bearer"},
+            # )
+            urls = ["/login", "/signup", "/", "/docs", "/openapi.json", "/redoc"]
+            print(request.url.path)
+            print(request.headers)
+            if (request.url.path not in urls):
+                token = request.headers.get('Authorization', None)
+                username = request.url.path.split('/')[1]
+
+                if (not token) or (not verify_access_token(token, username)):
+                    return JSONResponse(content={
+                            "message": "User Not logged in"
+                        }, status_code=status.HTTP_400_BAD_REQUEST)
+            try:
+                return await original_route_handler(request)
+            except RequestValidationError as exc:
+                    body = await request.body()
+                    detail = {"errors": exc.errors(), "body": body.decode()}
+                    raise HTTPException(status_code=422, detail=detail)
+        return route_authentication_handler
 #JWT STUFF
+#CORS STUFF
+origins = [
+    "http://localhost:3000"
+    ]
+
+middleware = [
+    Middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+]
+
+app = FastAPI(middleware=middleware)
+app.router.route_class = IsLoggedIn
+#CORS STUFF
 
 @app.get("/")
 def root():
@@ -121,14 +134,14 @@ def signup(user: User, response: Response, db: Session = Depends(get_db)):
     # check the db to see if user with this email exists
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-          message = {"message": "user with the email already exists"}
+          message = {"message": "User with the email already exists"}
           response.status_code = status.HTTP_302_FOUND  
           return message
     
     # check the db to see if user with this username already exists
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
-        message = {"message": "user with the username already exists"}
+        message = {"message": "User with the username already exists"}
         response.status_code = status.HTTP_302_FOUND
         return message
 
@@ -153,7 +166,7 @@ def signup(user: User, response: Response, db: Session = Depends(get_db)):
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return message
     
-    # TODO: generate JWT token and send it as header along with the 200 ok status
+    # generate JWT token and send it as header along with the 200 ok status
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
@@ -191,20 +204,62 @@ def login(user: User, response: Response, db: Session = Depends(get_db)):
         return message
     
     #is the correct user
-    # TODO: generate JWT token and send it as header along with the 200 ok status
+    # generate JWT token and send it as header along with the 200 ok status
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    
-    message = {"message": "User loggedin Successfully",
+    message = {"message": "User logged in successfully",
                 "username": user.username,
                 "access_token": access_token, 
                 "token_type": "bearer"}
     response.status_code = status.HTTP_200_OK
     return message  
 
-# TODO: verify JWT before accesssing this page
+
 @app.get("/{username}")
-def home(username: str, request: Request):
-    return {"home": username}
+def home(username: str, db: Session=Depends(get_db)):
+    return {"message": crud.get_user_goals(db=db, username=username)}
+
+
+@app.post("/{username}/create_specific_goal")
+def create_specific_goal(username: str, goal: schemas.GoalSpecificCreate,
+                response: Response, db: Session=Depends(get_db)):
+    #session authentication
+    user = crud.get_user_by_username(db=db, username=username)
+    if not user:
+        message = {"message": "User not found"}
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return message
+    creator_id = user.id
+    crud.create_specific_goal(db=db, goal=goal, user_id=creator_id)
+    template=crud.get_template(db=db, template_id=goal.template_id)
+    if not template:
+        message = {"message": "error: template not found"}
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return message
+    message = {"message": "Goal successfully created!"}
+    response.status_code = status.HTTP_200_OK
+    return message
+
+@app.post("/{username}/create_template")
+def create_template(username: str, template: schemas.TemplateCreate,
+                    response: Response, db: Session=Depends(get_db)):
+    user = crud.get_user_by_username(db=db, username=username)
+    if not user:
+        message = {"message": "error: user not found"}
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return message
+    creator_id = user.id
+    crud.create_template(db=db, template=template, creator_id=creator_id)
+    message = {"message": "template successfully created!"}
+    return message
+
+
+@app.get("/{username}/goals")
+def view_goals():
+    return "kazoink!"
+
+@app.get("/{username}/templates", response_model=list[schemas.Template])
+def view_premade_templates(db: Session=Depends(get_db), skip: int = 0, limit: int = 100):
+    return crud.get_premade_templates(db=db, skip=skip, limit=limit)
