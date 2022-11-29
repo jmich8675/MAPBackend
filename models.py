bcef3617fd15ec566be243b577cdda0863b86d79
b8man <edwar313@purdue.edu>
2022-11-29 18:00:59 -0500

    verifcation date in user table

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ class User(Base):
     pw_salt = Column(String)
     email = Column(String, unique=True, index=True)
     is_verified = Column(Boolean, default=False, index=True)
+    verification_sent = Column(Date)
 
     goals = relationship("Goal", back_populates="creator")
     myposts = relationship("Post", back_populates="poster")
