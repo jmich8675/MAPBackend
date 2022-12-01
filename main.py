d2e1bc7d12fa6b1589e0c5590f6c765f6344e74e
b8man <edwar313@purdue.edu>
2022-11-30 19:56:54 -0500

    view public status

--- main.py
+++ main.py
@@ -500,7 +500,8 @@ def view_goal_progress(goal_id: int, response: Response, db: Session = Depends(g
         "check_in_period": goal.check_in_period,
         "next_check_in": goal.next_check_in,
         "is_achieved": goal.is_achieved,
-        "is_paused": goal.is_paused
+        "is_paused": goal.is_paused,
+        "is_public": goal.is_public
     }
     return message
 
